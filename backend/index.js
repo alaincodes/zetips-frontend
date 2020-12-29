b59e4050f9b1b0dada6c0aca05e2d4ace9adf1f3
Alain Tran <alaint77@gmail.com>
2020-12-29 19:44:09 +0100

    add uuid to generate random ID and momentjs to format date

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+const moment = require('moment');
 
 const app = express();
 const cors = require('cors');
@@ -10,55 +12,46 @@ let questions = [
   {
     id: 1,
     author: 'Alain',
-    title: '2019-05-30T17:30:31.098Z',
-    body: 'Cupcake ipsum dolor. Sit amet dragée. Jelly beans apple pie marshmallow wafer jelly beans soufflé icing bear claw ?',
-    creation_date: '2019-05-30T17:30:31.098Z',
+    content: 'Cupcake ipsum dolor. Sit amet dragée. Jelly beans apple pie marshmallow wafer jelly beans soufflé icing bear claw ?',
+    creation_date: 'Tuesday, December 29th 2020, 7:34:19 pm',
     closed_date: '2019-05-30T17:30:31.098Z',
+    tags: ['Javascript', 'Python', 'Java'],
   },
   {
     id: 2,
     author: 'Rag',
-    title: '2019-05-30T17:30:31.098Z',
-    body: 'Biscuit tootsie roll liquorice tart icing cookie muffin. Marzipan powder caramels apple pie jelly-o bonbon topping ?',
-    creation_date: '2019-05-30T17:30:31.098Z',
+    content: 'Biscuit tootsie roll liquorice tart icing cookie muffin. Marzipan powder caramels apple pie jelly-o bonbon topping ?',
+    creation_date: 'Tuesday, December 29th 2020, 7:34:19 pm',
     closed_date: '2019-05-30T17:30:31.098Z',
   },
   {
     id: 3,
     author: 'Ronaldo',
-    title: '2019-05-30T17:30:31.098Z',
-    body: 'Fruitcake chocolate bar ice cream danish candy jelly beans apple pie bear claw jelly. Caramels powder cheesecake ?',
-    creation_date: '2019-05-30T17:30:31.098Z',
+    content: 'Fruitcake chocolate bar ice cream danish candy jelly beans apple pie bear claw jelly. Caramels powder cheesecake ?',
+    creation_date: 'Tuesday, December 29th 2020, 7:34:19 pm',
     closed_date: '2019-05-30T17:30:31.098Z',
   },
 ];
 
 app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>');
+  response.send('<h1>zetips backend api</h1>');
 });
 
 app.get('/api/questions', (request, response) => {
-  const note = request.body;
+  const question = request.body;
   response.json(questions);
 });
 
 app.get('/api/questions/:id', (request, response) => {
   const id = Number(request.params.id);
-  const note = questions.find((note) => note.id === id);
+  const question = questions.find((question) => question.id === id);
 
-  if (note) {
-    response.json(note);
+  if (question) {
+    response.json(question);
   } else {
     response.status(404).end();
   }
 });
-
-const generateId = () => {
-  const maxId = questions.length > 0
-    ? Math.max(...questions.map((n) => n.id))
-    : 0;
-  return maxId + 1;
-};
 
 app.post('/api/questions', (request, response) => {
   const { body } = request;
@@ -69,21 +62,21 @@ app.post('/api/questions', (request, response) => {
     });
   }
 
-  const note = {
+  const question = {
+    id: uuidv4(),
+    author: body.author,
     content: body.content,
-    important: body.important || false,
-    date: new Date(),
-    id: generateId(),
+    creation_date: moment().format('dddd, MMMM Do YYYY, h:mm:ss a'),
   };
 
-  questions = questions.concat(note);
+  questions = questions.concat(question);
 
-  response.json(note);
+  return response.json(question);
 });
 
 app.delete('/api/questions/:id', (request, response) => {
   const id = Number(request.params.id);
-  questions = questions.filter((note) => note.id !== id);
+  questions = questions.filter((question) => question.id !== id);
 
   response.status(204).end();
 });
